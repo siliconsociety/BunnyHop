c9330a7a06249a9116008b0f9b2657151da43ce4
BrokenShell <webmaster@sharpdesigndigital.com>
2025-04-26 11:03:08 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import asyncio
+
+import httpx
+import aiofiles
+from dotenv import load_dotenv
+
+
+class BunnyHop:
+    load_dotenv()
+    api_key = os.getenv("BUNNY_API_KEY")
+    lib_id = os.getenv("BUNNY_LIB_ID")
+    base_url = f"https://video.bunnycdn.com/library/{lib_id}/videos"
+    headers = {"accept": "application/json", "AccessKey": api_key}
+
+    async def __call__(self, title: str, filepath: str):
+        async with httpx.AsyncClient() as client:
+            resp = await client.post(self.base_url, headers=self.headers, json={"title": title})
+            resp.raise_for_status()
+            vid_id = resp.json()["guid"]
+            upload_url = f"{self.base_url}/{vid_id}"
+            async with aiofiles.open(filepath, "rb") as f:
+                data = await f.read()
+            resp = await client.put(upload_url, headers=self.headers, content=data)
+            resp.raise_for_status()
+            return vid_id
+
+
+if __name__ == "__main__":
+    async def main():
+        bunny = BunnyHop()
+        vid_id = await bunny(title="Testing Video Upload", filepath="videos/screen-0.mp4")
+        print(f"Video Upload Complete: {vid_id}")
+
+
+    asyncio.run(main())
